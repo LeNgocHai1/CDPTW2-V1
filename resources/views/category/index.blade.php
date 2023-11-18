fb7926526e4bd0d8315b6d8408f08e97f48580e7
dien1925 <86411904+dien1925@users.noreply.github.com>
2023-11-18 15:03:55 +0700

    dsCategory

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -7,66 +7,25 @@
       <thead class="bg-info text-white"> 
         <tr>
           <th>Các danh mục sản phẩm</th>
-          
           <th>Thao tác</th>
         </tr>
       </thead>
       <tbody>
+        @foreach ($categories as $category)
            <tr>
-            <td>Mainboard</td>
+            <td>{{ $category->categoryName }}</td>
             
-            <td> <a class="button btn btn-danger " href="{{ route('product.show') }}"><i class="fas fa-info-circle"></i>  Chi tiết</a>
-              <a class="button btn btn-success" href="{{ route('category.edit') }}"><i class="fas fa-tools"></i>  Sửa</a>
-              <form class="d-inline-block " action="{{ route('category.destroy') }}" method="post" >
+            <td> <a class="button btn btn-danger " href="{{ route('product.show',$category->categoryID) }}"><i class="fas fa-info-circle"></i>  Chi tiết</a>
+              <a class="button btn btn-success" href="{{ route('category.edit',$category->categoryID) }}"><i class="fas fa-tools"></i>  Sửa</a>
+              <form class="d-inline-block " action="{{ route('category.destroy', $category->categoryID) }}" method="post" >
                 {{ csrf_field() }}
                 @method('DELETE')
-                {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-                {{-- or --}}
-                {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-                {{-- <input type="hidden" name="_method" value="delete"> --}}
-                
-                <input type="submit" value="Xóa" class="button btn btn-secondary">
+                <button type="submit" onclick="return confirm('Bạn có chắc chắn muốn xóa danh mục này?')" class="button btn btn-secondary">Xóa</button>
                 </form>
             
             </td>
           </tr>
-          <tr>
-            <td>Cpu</td>
-            
-            <td> <a class="button btn btn-danger " href="{{ route('product.show') }}"><i class="fas fa-info-circle"></i>  Chi tiết</a>
-              <a class="button btn btn-success" href="{{ route('category.edit') }}"><i class="fas fa-tools"></i>  Sửa</a>
-              <form class="d-inline-block " action="{{ route('category.destroy') }}" method="post" >
-                {{ csrf_field() }}
-                @method('DELETE')
-                {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-                {{-- or --}}
-                {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-                {{-- <input type="hidden" name="_method" value="delete"> --}}
-                
-                <input type="submit" value="Xóa" class="button btn btn-secondary">
-                </form>
-            
-            </td>
-          </tr>
-          <tr>
-            <td>RAM</td>
-            
-            <td> <a class="button btn btn-danger " href="{{ route('product.show') }}"><i class="fas fa-info-circle"></i>  Chi tiết</a>
-              <a class="button btn btn-success" href="{{ route('category.edit') }}"><i class="fas fa-tools"></i>  Sửa</a>
-              <form class="d-inline-block " action="{{ route('category.destroy') }}" method="post" >
-                {{ csrf_field() }}
-                @method('DELETE')
-                {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-                {{-- or --}}
-                {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-                {{-- <input type="hidden" name="_method" value="delete"> --}}
-                
-                <input type="submit" value="Xóa" class="button btn btn-secondary">
-                </form>
-            
-            </td>
-          </tr>
- 
+          @endforeach
 
       </tbody>
     </table>
