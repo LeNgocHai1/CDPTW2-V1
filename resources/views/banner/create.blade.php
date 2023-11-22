a97f6aeec6d5db9563a4069520564b9517fca64c
dien1925 <86411904+dien1925@users.noreply.github.com>
2023-11-22 23:22:13 +0700

    allrepair

--- resources/views/banner/create.blade.php
+++ resources/views/banner/create.blade.php
@@ -1,19 +1,18 @@
 @extends('layout.admin.main')
 @section('content')
 <h5 style="font-weight: bold">Thêm ảnh cho Banner quảng cáo</h5>
-
+<div class="card-body">
+    @include('error')
+ </div>
 <div class="container">
+    @if(isset($banner))
     <div class="row">
         <div class="col-sm-10">
-            <form action="{{ route('banner.store') }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('banner.store') }}" method="post" enctype="multipart/form-data" id="productForm">
             {{ csrf_field() }}  
             <div class="form-group">
                 <label for="categoryID" style="font-weight: bold">Hình ảnh:</label>
-               
-
             </div>
-           
-            
             <div class="form-group">
                 <label for="slide1" style="font-weight: bold">Chọn hình ảnh cho slide:</label>
                 <input type="file" name="slide1" id="slide1" class="form-control-file"> 
@@ -23,11 +22,20 @@
                 <input type="file" name="ads" id="ads" class="form-control-file"> 
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-info btn-sm" value="Tạo sản phẩm">
+                <input type="submit" id="submitButton" class="btn btn-info btn-sm" value="Tạo sản phẩm">
             </div>
             </form>
         </div>
     </div>
+    @else
+    <p>Sản phẩm không tồn tại.</p>
+@endif
 </div>
-
+<script>
+     $(document).ready(function() {
+            $('#productForm').submit(function() {
+                $('#submitButton').prop('disabled', true);
+            });
+        });
+     </script>
 @endsection
--- resources/views/productdetail/create.blade.php
+++ resources/views/productdetail/create.blade.php
@@ -1,27 +1,34 @@
 @extends('layout.admin.main')
 @section('content')
 <h5 style="font-weight: bold">Thêm chi tiết sản phẩm</h5>
+<div class="card-body">
+    @include('error')
+ </div>
 <div class="container">
     <div class="row">
         <div class="col-sm-10">
-            <form action="{{ route('prodetail.store') }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('prodetail.store') }}" method="post"  onsubmit="return validateForm()" enctype="multipart/form-data">
             {{ csrf_field() }}  
             <div class="form-group">
                 <label for="categoryID" style="font-weight: bold">Sản phẩm:</label>
                 <select name="productID" id="productID" class="form-control">
-                    <option value="">
-                        CPU INTEL Core i7-9700 (8C/8T, 3.00 GHz up to 4.70 GHz, 12MB) - 1151-v2
+                    @foreach ($products as $product)
+                    <option value="{{ $product->productID }}">
+                    {{ $product->productName }}
                     </option>
+                        
+                    @endforeach
                 </select>
 
             </div>
             <div class="form-group">
                 <label for="productName" style="font-weight: bold">Tên thương hiệu</label>
                 <input type="text" name="brand" id="brand" class="form-control">
+                <span id="charCount" class="text-danger"></span>
             </div>
             <div class="form-group">
                 <label for="productName" style="font-weight: bold">Thời gian bảo hành</label>
-                <input type="text" name="guarantee" id="guarantee" class="form-control">
+                <input type="text" name="guarantee" id="guarantee" class="form-control" pattern="[0-9]+>
             </div>
             <div class="form-group">
                 <label for="productImage1" style="font-weight: bold">Chọn hình ảnh thứ nhất :</label>
@@ -40,22 +47,44 @@
                 <textarea name="description" class="form-control summernote" id="summernote"></textarea>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-info btn-sm" value="lưu thay đổi">
+                <input type="submit" id="submitButton" class="btn btn-info btn-sm" value="Thêm chi tiết sản phẩm">
             </div>
             </form>
         </div>
     </div>
 </div>
 <script>
-    $(document).ready(function(){
-   
-   $('.summernote').summernote({
-       height: 240,
-       minHeight: null,
-       maxHeight: null,
-       focus: false
-   });
-   
-   });
-     </script>
+    $(document).ready(function() {
+        $('.summernote').summernote({
+            height: 240,
+            minHeight: null,
+            maxHeight: null,
+            focus: false
+        });
+
+
+        $('#brand').on('input', function() {
+            var maxLength = 255; 
+            var currentLength = $(this).val().length;
+
+            if (currentLength > maxLength) {
+                $('#charCount').text('Vui lòng không nhập quá ' + maxLength + ' ký tự.');
+            } else {
+                $('#charCount').text('');
+            }
+        });
+    });
+
+    function validateForm() {
+        var maxLength = 255; 
+        var currentLength = $('#brand').val().length;
+
+        if (currentLength > maxLength) {
+            alert('Vui lòng không nhập quá ' + maxLength + ' ký tự.');
+            return false; 
+        }
+
+        $('#submitButton').prop('disabled', true);
+    }
+</script>
 @endsection
