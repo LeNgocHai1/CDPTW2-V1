85a3e3df80bc97fee0be044a0040cb65dc2043c9
gifcuong <20211tt2559@mail.tdc.edu.vn>
2023-11-23 14:03:57 +0700

    updatetonghop

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,22 +13,39 @@ class AdminController extends Controller
     public function index()
    {
    
-        return view('admin');
+   $sql = "select * from v_quantity";
+   $quantity = DB::select($sql);
+
+        return view('admin',['quantity'=>$quantity]);
     }
      public function find(Request $request)
     {
-      
-       return view('product.find');
+  
+       if(!is_numeric($request->keywords))
+       {
+           $result = DB::table('categories')
+           ->join('products','categories.categoryID','=','products.categoryID')
+           ->select('categoryName','productName','productImage','listPrice')
+           ->orWhere('categoryName','like','%'.$request->keywords.'%')
+           ->orWhere('productName','like','%'.$request->keywords.'%')
+           ->get();
+       }
+       else{
+           $result = DB::table('categories')
+           ->join('products','categories.categoryID','=','products.categoryID')
+           ->select('categoryName','productName','productImage','listPrice')
+           ->Where('listPrice','>',$request->keywords) 
+           ->get();         
+       }
+       
+       return view('product.find',['result'=>$result]);
         
-
-         
-
 
     }
     public function count_category()
     {
         $count = Users::count('name');
-         return view('layout.admin.main');
+         return view('layout.admin.main',['count'=>$count]);
     }
     
     public function __construct()
