7a0ac73e0c1c4fba35122c77e039ff6f7e550135
LeNgocHai1 <lengochai.fit2019@gmail.com>
2023-11-23 14:29:27 +0700

    push

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -15,78 +15,32 @@
         </tr>
       </thead>
       <tbody>
+       @foreach ($products as $product)
        <tr>
         <td>
-          CPU INTEL Core i7-9700 (8C/8T, 3.00 GHz up to 4.70 GHz, 12MB) - 1151-v2
+           {{ $product->productName }} 
         <td>
-             <div class="product-image-thumb" ><img src="../public/upload/cpu3.jpg" alt="Product Image"></div>
+             <div class="product-image-thumb" ><img src="{{ asset('public/upload/'. $product->productImage) }}" alt="Product Image"></div>
         </td>
         <td>
-          2.644.000 VNĐ
+          {{number_format($product->listPrice)}}đ
         </td>
-        <td>10%</td>
+        <td>{{ $product->discountPercent }}%</td>
         <td> 
-            <a class="button btn btn-success" href=""><i class="fas fa-tools"></i>  Sửa</a>
-            <form class="d-inline-block " action="" method="post" >
-              {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-              {{-- or --}}
-              {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-              {{-- <input type="hidden" name="_method" value="delete"> --}}
-              
-              <input type="submit" value="Xóa" class="button btn btn-danger">
-              </form>
+          <a class="button btn btn-danger" href="{{ route('product.showdetail', $product->productID) }}"><i class="fas fa-info-circle"></i> Chi tiết</a>
+          <a class="button btn btn-success" href="{{ route('product.edit',$product->productID) }}"><i class="fas fa-tools"></i>  Sửa</a>
+          <form class="d-inline-block " action="{{ route('product.destroy',$product->productID) }}" method="post" >
+            {{ csrf_field() }}
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Bạn có chắc chắn muốn xóa?')" class="button btn btn-danger">Xóa</button>
+            </form>
           </td>
       </tr>
-      <tr>
-        <td>
-          CPU INTEL Core i7-9700 (8C/8T, 3.00 GHz up to 4.70 GHz, 12MB) - 1151-v2
-        <td>
-             <div class="product-image-thumb" ><img src="../public/upload/cpu3.jpg" alt="Product Image"></div>
-        </td>
-        <td>
-          2.644.000 VNĐ
-        </td>
-        <td>10%</td>
-        <td> 
-            <a class="button btn btn-success" href=""><i class="fas fa-tools"></i>  Sửa</a>
-            <form class="d-inline-block " action="" method="post" >
-              {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-              {{-- or --}}
-              {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-              {{-- <input type="hidden" name="_method" value="delete"> --}}
-              
-              <input type="submit" value="Xóa" class="button btn btn-danger">
-              </form>
-          </td>
-      </tr>
-      <tr>
-        <td>
-          CPU INTEL Core i7-9700 (8C/8T, 3.00 GHz up to 4.70 GHz, 12MB) - 1151-v2
-        <td>
-             <div class="product-image-thumb" ><img src="../public/upload/cpu3.jpg" alt="Product Image"></div>
-        </td>
-        <td>
-          2.644.000 VNĐ
-        </td>
-        <td>10%</td>
-        <td> 
-            <a class="button btn btn-success" href=""><i class="fas fa-tools"></i>  Sửa</a>
-            <form class="d-inline-block " action="" method="post" >
-              {{-- HTML không có các method PUT, PATCH, DELETE, nên cần dùng lệnh @method để có thể gán các method này --}}
-              {{-- or --}}
-              {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> --}}
-              {{-- <input type="hidden" name="_method" value="delete"> --}}
-              
-              <input type="submit" value="Xóa" class="button btn btn-danger">
-              </form>
-          </td>
-      </tr>
-       
-
+       @endforeach
 
       </tbody>
     </table>
-    <div class="d-flex justify-content-center"></div>
+    <div class="d-flex justify-content-center">{{ $products->links() }}</div>
     <p class="d-flex justify-content-end">
         <a class="btn btn-info btn-sm fa fa-plus" href="{{ route('product.create') }}">Thêm Sản phẩm</a>
     </p>
